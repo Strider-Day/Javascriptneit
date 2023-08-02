4a5b48cfc2e3ea901c53ea61c9123627f04780eb
Andrew Rockwell <ajrockwell@email.neit.edu>
2023-08-01 21:07:39 -0400

    w2 hw (not done)

--- w1/js/player.js
+++ w1/js/player.js
@@ -1,3 +1,6 @@
+var c = document.querySelector(`#pong`)
+var ctx = c.getContext(`2d`)
+
 class Player
 {
     constructor()
@@ -5,6 +8,34 @@ class Player
         this.name = "yes"
         this.score = 0
         this.highscore = 0
-        this.pad = "red"
+        this.pad = ""
     }
 }
+
+var player = []
+
+var pad = []
+
+player[0] = new Player();
+player[0].pad = new Box();
+
+pad[0] = player[0].pad
+pad[0].w = 20
+pad[0].x = 0 + pad[0].w/2
+pad[0].h = 150
+pad[0].color = `red`
+
+player[1] = new Player();
+player[1].pad = new Box();
+
+pad[1] = player[1].pad
+pad[1].w = 20
+pad[1].x = c.width - pad[1].w/2
+pad[1].h = 150
+pad[1].color = `red`
+
+
+
+
+
+
